fb3a2d534a60d9f534315f1bd455a808cdcefd8e
Julien HENRY <julien.henry@sonarsource.com>
2016-01-13 22:11:17 +0100

    Update build config

--- travis.sh
+++ travis.sh
@@ -14,19 +14,18 @@ CI)
     strongEcho 'Build and analyze commit in master'
     # this commit is master must be built and analyzed (with upload of report)
 
-    ./gradlew sonarqube \
+    ./gradlew build check sonarqube \
         -Dsonar.host.url=$SONAR_HOST_URL \
         -Dsonar.login=$SONAR_TOKEN
 
   elif [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ -n "$GITHUB_TOKEN" ]; then
-    # For security reasons environment variables are not available on the pull requests
-    # coming from outside repositories
+    # For security reasons environment variables are not available on the pull requests coming from outside repositories
     # http://docs.travis-ci.com/user/pull-requests/#Security-Restrictions-when-testing-Pull-Requests                                                                         
-    # That's why the analysis does not need to be executed if the variable SONAR_GITHUB_OAUTH is not defined.                                                                
+    # That's why the analysis does not need to be executed if the variable GITHUB_TOKEN is not defined.                                                                
                                                                                                                                                                              
     strongEcho 'Build and analyze pull request'                                                                                                                              
 
-    ./gradlew sonarqube \
+    ./gradlew build check sonarqube \
         -Dsonar.analysis.mode=issues \
         -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST \
         -Dsonar.github.repository=$TRAVIS_REPO_SLUG \
@@ -38,7 +37,7 @@ CI)
     strongEcho 'Build, no analysis'
     # Build branch, without any analysis
 
-    ./gradlew build install 
+    ./gradlew build check
   fi
   ;;
 
