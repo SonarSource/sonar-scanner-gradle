41886547c4110d81255d1fca78314a0eed8cdfcb
Tom <thomas.verin@sonarsource.com>
2016-05-11 17:36:15 +0200

    handle PR deploy and Release branch in travis.sh

--- travis.sh
+++ travis.sh
@@ -9,7 +9,7 @@ function strongEcho {
 
 function configureTravis {
   mkdir -p ~/.local
-  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v27 | tar zx --strip-components 1 -C ~/.local
+  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v28 | tar zx --strip-components 1 -C ~/.local
   source ~/.local/bin/install
 }
 configureTravis
@@ -20,24 +20,6 @@ if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; th
   strongEcho 'Build and analyze commit in master'
   # this commit is master must be built and analyzed (with upload of report)
 
-  ./gradlew build check sonarqube \
-      -Dsonar.host.url=$SONAR_HOST_URL \
-      -Dsonar.login=$SONAR_TOKEN
-
-elif [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ "$TRAVIS_SECURE_ENV_VARS" == "true" ]; then
-  strongEcho 'Build and analyze pull request'                                                                                                                              
-
-  ./gradlew build check sonarqube \
-      -Dsonar.analysis.mode=issues \
-      -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST \
-      -Dsonar.github.repository=$TRAVIS_REPO_SLUG \
-      -Dsonar.github.oauth=$GITHUB_TOKEN \
-      -Dsonar.host.url=$SONAR_HOST_URL \
-      -Dsonar.login=$SONAR_TOKEN
-
-elif [ "$TRAVIS_BRANCH" == "feature/cix" ]; then
-  strongEcho 'feature/cix: build analyse deploy on repox'
-  
   # Analyze with SNAPSHOT version as long as SQ does not correctly handle
   # purge of release data
   CURRENT_VERSION=`cat gradle.properties | grep version | awk -F= '{print $2}'`
@@ -47,12 +29,61 @@ elif [ "$TRAVIS_BRANCH" == "feature/cix" ]; then
   echo buildInfo.build.name=sonar-gradle >> gradle.properties 
   echo buildInfo.build.number=$TRAVIS_BUILD_NUMBER >> gradle.properties 
 
-
-  ./gradlew artifactory check sonarqube \
+  ./gradlew build check sonarqube \  
       -Dsonar.projectVersion=$CURRENT_VERSION \
       -Dsonar.host.url=$SONAR_HOST_URL \
       -Dsonar.login=$SONAR_TOKEN
 
+elif [[ "${TRAVIS_BRANCH}" == "branch-"* ]] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+  # no dory analysis on release branch
+  if [[ $CURRENT_VERSION =~ "-SNAPSHOT" ]]; then
+    echo "======= Found SNAPSHOT version ======="
+    # Do not deploy a SNAPSHOT version but the release version related to this build
+    # Do not deploy a SNAPSHOT version but the release version related to this build
+    sed -i.bak "s/-SNAPSHOT/-build$TRAVIS_BUILD_NUMBER/g" gradle.properties    
+    # set the build name with travis build number
+    echo buildInfo.build.name=sonar-gradle >> gradle.properties 
+    echo buildInfo.build.number=$TRAVIS_BUILD_NUMBER >> gradle.properties 
+  else
+    echo "======= Found RELEASE version ======="
+  fi     
+  #build and deploy
+  ./gradlew build artifactory 
+
+elif [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ "$TRAVIS_SECURE_ENV_VARS" == "true" ]; then
+  strongEcho 'Build and analyze pull request'  
+
+  if [ "${DEPLOY_PULL_REQUEST:-}" == "true" ]; then
+    echo '======= with deploy'
+
+    # Analyze with SNAPSHOT version as long as SQ does not correctly handle
+    # purge of release data
+    CURRENT_VERSION=`cat gradle.properties | grep version | awk -F= '{print $2}'`
+    # Do not deploy a SNAPSHOT version but the release version related to this build
+    sed -i.bak "s/-SNAPSHOT/-build$TRAVIS_BUILD_NUMBER/g" gradle.properties    
+    # set the build name with travis build number
+    echo buildInfo.build.name=sonar-gradle >> gradle.properties 
+    echo buildInfo.build.number=$TRAVIS_BUILD_NUMBER >> gradle.properties 
+    
+    ./gradlew build check sonarqube artifactory \
+      -Dsonar.analysis.mode=issues \
+      -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST \
+      -Dsonar.github.repository=$TRAVIS_REPO_SLUG \
+      -Dsonar.github.oauth=$GITHUB_TOKEN \
+      -Dsonar.host.url=$SONAR_HOST_URL \
+      -Dsonar.login=$SONAR_TOKEN
+
+  else
+    echo '======= no deploy'                                                                                                                            
+
+    ./gradlew build check sonarqube \
+      -Dsonar.analysis.mode=issues \
+      -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST \
+      -Dsonar.github.repository=$TRAVIS_REPO_SLUG \
+      -Dsonar.github.oauth=$GITHUB_TOKEN \
+      -Dsonar.host.url=$SONAR_HOST_URL \
+      -Dsonar.login=$SONAR_TOKEN
+  fi
 else
   strongEcho 'Build, no analysis'
   # Build branch, without any analysis
