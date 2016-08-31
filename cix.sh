29fe78fe8dd360f672f304263850e74bfd4441c8
Julien HENRY <julien.henry@sonarsource.com>
2016-08-31 10:11:54 +0200

    Enable stacktrace in ITs

--- cix.sh
+++ cix.sh
@@ -3,5 +3,5 @@ set -euo pipefail
 echo "Running with GRADLE_VERSION=$GRADLE_VERSION"
     
 cd integrationTests
-mvn clean verify -Dgradle.version=$GRADLE_VERSION
+mvn -e -B clean verify -Dgradle.version=$GRADLE_VERSION
 
