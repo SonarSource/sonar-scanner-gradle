60492927d34a92e5c2e90eef7fc89bdb4bc2b358
Julien HENRY <julien.henry@sonarsource.com>
2017-10-30 10:11:57 +0100

    Automatically install Android license before running QA

--- cix.sh
+++ cix.sh
@@ -3,5 +3,6 @@ set -euo pipefail
 echo "Running with GRADLE_VERSION=$GRADLE_VERSION"
     
 cd integrationTests
+mkdir -p $ANDROID_HOME/licenses
+cp -f licenses/* $ANDROID_HOME/licenses
 mvn -e -B clean verify -Dgradle.version=$GRADLE_VERSION
-
