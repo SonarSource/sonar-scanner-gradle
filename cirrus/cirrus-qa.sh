a78d9d4ea986183510cc1ffd04295e7a6597a4ec
Julien Lancelot <julien.lancelot@sonarsource.com>
2020-03-12 18:09:27 +0100

    Fix build version of ITs

--- cirrus/cirrus-qa.sh
+++ cirrus/cirrus-qa.sh
@@ -6,6 +6,9 @@ echo "ANDROID_HOME="$ANDROID_HOME
 
 cd integrationTests
 
+# Make sure ITs are using the same version as the plugin
+source set_maven_build_version $BUILD_NUMBER
+
 mkdir -p $ANDROID_HOME/licenses
 cp -f licenses/* $ANDROID_HOME/licenses
 
