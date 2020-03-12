37e5a3a585fac91df61868b240848a02dced70c9
Julien Lancelot <julien.lancelot@sonarsource.com>
2020-03-12 18:10:12 +0100

    Ignore ANDROiD_HOME for the moment

--- cirrus/cirrus-qa.sh
+++ cirrus/cirrus-qa.sh
@@ -2,15 +2,15 @@
 
 source cirrus-env QA
 
-echo "ANDROID_HOME="$ANDROID_HOME
+#echo "ANDROID_HOME="$ANDROID_HOME
 
 cd integrationTests
 
 # Make sure ITs are using the same version as the plugin
 source set_maven_build_version $BUILD_NUMBER
 
-mkdir -p $ANDROID_HOME/licenses
-cp -f licenses/* $ANDROID_HOME/licenses
+#mkdir -p $ANDROID_HOME/licenses
+#cp -f licenses/* $ANDROID_HOME/licenses
 
 mvn -e -B clean verify -Dgradle.version=$GRADLE_VERSION
 
