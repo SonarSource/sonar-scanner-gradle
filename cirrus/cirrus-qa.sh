3a046549eba54efb9a3b283f302d0285565cfdff
Julien Lancelot <julien.lancelot@sonarsource.com>
2020-03-12 17:55:09 +0100

    Try QA

--- cirrus/cirrus-qa.sh
+++ cirrus/cirrus-qa.sh
@@ -1,30 +1,12 @@
 #!/bin/bash
 
-. ./private/cirrus/cirrus-env.sh QA
+source cirrus-env QA
 
-if [[ "$1" == oracle* ]]; then
-  # Need ~ 10 minutes to have a running instance of Oracle
-  sleep 600
-fi
+cd integrationTests
+mkdir -p $ANDROID_HOME/licenses
+cp -f licenses/* $ANDROID_HOME/licenses
+mvn -e -B clean verify -Dgradle.version=$GRADLE_VERSION
 
-ORCHESTRATOR_CONFIG=$1
 
-ITS_TASK=":private:it-core:integrationTest -Dcategory=${QA_CATEGORY}"
-if [ "${QA_CATEGORY}" == "Gov" ]; then
-	ITS_TASK=':private:it-governance:it-tests:integrationTest'
-fi
-if [ "${QA_CATEGORY}"  == "Branch" ]; then
-	ITS_TASK=':private:it-branch:it-tests:integrationTest'
-fi
-if [ "${QA_CATEGORY}"  == "License" ]; then
-	ITS_TASK=':private:it-license:it-tests:integrationTest'
-fi
-if [ "${QA_CATEGORY}"  == "HA" ]; then
-        ITS_TASK=':private:it-ha:it-tests:integrationTest'
-fi
 
-gradle ${ITS_TASK} \
-	-DbuildNumber=$BUILD_NUMBER \
-	-Pqa -Ptime-tracker \
-	-Dorchestrator.configUrl=file:///$CIRRUS_WORKING_DIR/private/cirrus/orchestrator-$ORCHESTRATOR_CONFIG.properties \
-	--console plain --no-daemon --build-cache
+
