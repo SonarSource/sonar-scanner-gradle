bd919bdc729e50216774b574c4b4f1bfa2b352f8
Julien Lancelot <julien.lancelot@sonarsource.com>
2020-03-12 18:02:12 +0100

    Add debug

--- cirrus/cirrus-qa.sh
+++ cirrus/cirrus-qa.sh
@@ -2,9 +2,13 @@
 
 source cirrus-env QA
 
+echo "ANDROID_HOME="$ANDROID_HOME
+
 cd integrationTests
+
 mkdir -p $ANDROID_HOME/licenses
 cp -f licenses/* $ANDROID_HOME/licenses
+
 mvn -e -B clean verify -Dgradle.version=$GRADLE_VERSION
 
 
