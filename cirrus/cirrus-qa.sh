47bce9f48f9134fa6d9895c8a13ef1f3ba6d2a86
Julien Lancelot <julien.lancelot@sonarsource.com>
2020-03-13 11:53:58 +0100

    Add Android SDK

--- cirrus/cirrus-qa.sh
+++ cirrus/cirrus-qa.sh
@@ -2,6 +2,7 @@
 
 source cirrus-env QA
 
+# Set ITs to have the same version as the plugin
 CURRENT_VERSION=`cat gradle.properties | grep version | awk -F= '{print $2}'`
 RELEASE_VERSION=`echo $CURRENT_VERSION | sed "s/-.*//g"`
 number_dots=`echo $RELEASE_VERSION | grep -o "\." | wc -l`
@@ -16,10 +17,12 @@ cd integrationTests
 
 mvn org.codehaus.mojo:versions-maven-plugin:2.7:set -DnewVersion=$NEW_VERSION -DgenerateBackupPoms=false -B -e
 
-#echo "ANDROID_HOME="$ANDROID_HOME
-#mkdir -p $ANDROID_HOME/licenses
-#cp -f licenses/* $ANDROID_HOME/licenses
+# Simulate license acceptance
+echo "ANDROID_HOME="$ANDROID_HOME
+mkdir -p $ANDROID_HOME/licenses
+cp -f licenses/* $ANDROID_HOME/licenses
 
+# Execute ITs
 mvn -e -B clean verify -Dgradle.version=$GRADLE_VERSION
 
 
