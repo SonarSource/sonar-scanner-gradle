817e28a3bc1fa97a551e9c97ed5f2dd3206b1fc4
Julien Lancelot <julien.lancelot@sonarsource.com>
2020-03-13 10:01:28 +0100

    fix all

--- cirrus/maven_expression.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Evaluate a Maven expression
-# Usage: maven_expression "expression"
-# Example: maven_expression "project.version"
-
-#set -euo pipefail
-
-mvn -q -Dexec.executable="echo" -Dexec.args="\${$1}" --non-recursive org.codehaus.mojo:exec-maven-plugin:1.3.1:exec
