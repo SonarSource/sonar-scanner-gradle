2f7f49b13c47c57edf6f1d63da12a9642fe9e555
Duarte Meneses <duarte.meneses@sonarsource.com>
2022-10-26 11:56:11 -0500

    [NO JIRA] parse android plugin version

---
+++ src/main/java/org/sonarqube/gradle/Version.java
@@ -0,0 +1,97 @@
+/*
+ * SonarQube Scanner for Gradle
+ * Copyright (C) 2015-2022 SonarSource
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+package org.sonarqube.gradle;
+
+import java.util.Comparator;
+import java.util.Objects;
+import org.jetbrains.annotations.NotNull;
+
+public class Version implements Comparable<Version> {
+  private final int major;
+  private final int minor;
+
+  private Version(int major, int minor) {
+    this.major = major;
+    this.minor = minor;
+  }
+
+  public int major() {
+    return major;
+  }
+
+  public int minor() {
+    return minor;
+  }
+
+  public static Version of(int major, int minor) {
+    return new Version(major, minor);
+  }
+
+  public static Version of(String version) {
+    String[] split = version.split("\\.");
+    int major = 0;
+    int minor = 0;
+
+    if (split.length > 0) {
+      major = parseInt(split[0]);
+      if (split.length > 1) {
+        minor = parseInt(split[1]);
+      }
+    }
+    return new Version(major, minor);
+  }
+
+  private static int parseInt(String field) {
+    try {
+      return Integer.parseInt(field);
+    } catch (NumberFormatException e) {
+      return 0;
+    }
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Version version = (Version) o;
+    return major == version.major && minor == version.minor;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(major, minor);
+  }
+
+  @Override
+  public String toString() {
+    return major + "." + minor;
+  }
+
+  @Override
+  public int compareTo(@NotNull Version o) {
+    return Comparator.comparingInt(Version::major)
+      .thenComparing(Version::minor)
+      .compare(this, o);
+  }
+}
---
+++ src/test/groovy/org/sonarqube/gradle/VersionTest.java
@@ -0,0 +1,58 @@
+/*
+ * SonarQube Scanner for Gradle
+ * Copyright (C) 2015-2022 SonarSource
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+package org.sonarqube.gradle;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class VersionTest {
+  @Test
+  public void parses_versions() {
+    assertVersion(Version.of("1.2.3"), 1, 2);
+    assertVersion(Version.of("1"), 1, 0);
+    assertVersion(Version.of("11.2.3"), 11, 2);
+    assertVersion(Version.of(""), 0, 0);
+    assertVersion(Version.of("1.20"), 1, 20);
+    assertVersion(Version.of("1.invalid.3"), 1, 0);
+  }
+
+  @Test
+  public void equals_and_hashcode() {
+    assertThat(Version.of(1, 1)).isEqualTo(Version.of(1, 1));
+    assertThat(Version.of(1, 2)).isNotEqualTo(Version.of(1, 1));
+    assertThat(Version.of(2, 1)).isNotEqualTo(Version.of(1, 1));
+
+    assertThat(Version.of(1, 1)).hasSameHashCodeAs(Version.of(1, 1));
+  }
+
+  @Test
+  public void compares_versions() {
+    assertThat(Version.of(1, 2)).isGreaterThan(Version.of(1, 1));
+    assertThat(Version.of(2, 1)).isGreaterThan(Version.of(1, 1));
+    assertThat(Version.of(3, 2)).isGreaterThan(Version.of(2, 9));
+    assertThat(Version.of(1, 1)).isEqualTo(Version.of(1, 1));
+  }
+
+  private static void assertVersion(Version v, int expectedMajor, int expectedMinor) {
+    assertThat(v.major()).isEqualTo(expectedMajor);
+    assertThat(v.minor()).isEqualTo(expectedMinor);
+  }
+}
