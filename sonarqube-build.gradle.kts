fb9d4686d3a39d8d07d556aed59b8fa3ed2de6f7
Irina Batinic <117161143+irina-batinic-sonarsource@users.noreply.github.com>
2023-05-26 15:23:50 +0200

    Update sonarqube version to 4.1.0.3113 (#170)

--- sonarqube-build.gradle.kts
+++ sonarqube-build.gradle.kts
@@ -8,7 +8,7 @@ buildscript {
     }
 
     dependencies {
-        classpath("org.sonarsource.scanner.gradle:sonarqube-gradle-plugin:4.0.0.2929")
+        classpath("org.sonarsource.scanner.gradle:sonarqube-gradle-plugin:4.1.0.3113")
     }
 
     val projectTitle: String by project
