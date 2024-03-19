e9b7bb725652d5681cc8ac5a976f074c82d52bcf
margarita <margarita.nedzelska@sonarsource.com>
2024-03-19 11:15:50 +0100

    Enable Android ITs with Android Gradle plugin later than 7

--- integrationTests/src/test/java/org/sonarqube/gradle/AndroidTest.java
+++ integrationTests/src/test/java/org/sonarqube/gradle/AndroidTest.java
@@ -38,7 +38,6 @@ public class AndroidTest extends AbstractGradleIT {
   @BeforeClass
   public static void beforeAll() {
     assumeNotNull(getAndroidGradleVersion());
-    assumeTrue(getAndroidGradleVersion().isGreaterThan("7.0.0"));
   }
 
   @Test
