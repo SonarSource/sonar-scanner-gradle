c14f4b337aeb9153d2e7b9b7d2b60bb1d93151d1
Dorian Burihabwa <dorian.burihabwa@sonarsource.com>
2024-06-10 13:45:24 +0200

    [NO-JIRA] Upgrade sonar-scanner-java-library to version 2.16.3.1081

--- build.gradle.kts
+++ build.gradle.kts
@@ -70,7 +70,7 @@ repositories {
 }
 
 dependencies {
-    implementation("org.sonarsource.scanner.api:sonar-scanner-api:2.16.2.588")
+    implementation("org.sonarsource.scanner.api:sonar-scanner-api:2.16.3.1081")
     compileOnly("com.google.code.findbugs:jsr305:3.0.2")
     compileOnly("com.android.tools.build:gradle:8.1.1")
     compileOnly("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.21")
