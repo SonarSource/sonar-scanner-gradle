412545bc7c6af0eb0f73989651b724a081e36c2d
Björn Kautler <Bjoern@Kautler.net>
2025-05-01 14:01:58 +0200

    SCANGRADLE-174 Reliably detect plugin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -109,7 +109,7 @@ license {
     header = rootProject.file("HEADER")
     mapping("java", "SLASHSTAR_STYLE")
     strictCheck = true
-    exclude("**/version.txt")
+    exclude("**/*-version.txt")
 }
 
 jacoco {
@@ -208,7 +208,7 @@ artifactory {
 }
 
 tasks.processResources {
-    filesMatching("**/version.txt") {
+    filesMatching("org/sonarqube/gradle/sonarqube-gradle-plugin-version.txt") {
         expand(mapOf("version" to project.version))
     }
 }
