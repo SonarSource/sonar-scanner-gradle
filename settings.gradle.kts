600fd8f3623a926c37da7377909fe870e1d6d553
Alban Auzeill <alban.auzeill@sonarsource.com>
2025-05-14 07:46:12 +0200

    SCANGRADLE-221 Move Gradle "repositories" configuration into settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,33 @@
 rootProject.name = "sonarqube-gradle-plugin"
 
+pluginManagement {
+    // The environment variables ARTIFACTORY_PRIVATE_USERNAME and ARTIFACTORY_PRIVATE_PASSWORD are used on CI env
+    // If you have access to "repox.jfrog.io" you can add artifactoryUsername and artifactoryPassword to ~/.gradle/gradle.properties
+    val artifactoryUsername: String = System.getenv("ARTIFACTORY_PRIVATE_USERNAME") ?: providers.gradleProperty("artifactoryUsername").getOrElse("")
+    val artifactoryPassword: String = System.getenv("ARTIFACTORY_PRIVATE_PASSWORD") ?: providers.gradleProperty("artifactoryPassword").getOrElse("")
+    val repoxRepository: java.net.URI = uri("https://repox.jfrog.io/repox/" + (if (providers.gradleProperty("qa").isPresent()) "sonarsource-qa" else "sonarsource"))
+    repositories {
+        mavenLocal()
+        if (artifactoryUsername.isNotEmpty() && artifactoryPassword.isNotEmpty()) {
+            maven {
+                url = repoxRepository
+                credentials {
+                    username = artifactoryUsername
+                    password = artifactoryPassword
+                }
+            }
+        } else {
+            mavenCentral()
+        }
+        gradlePluginPortal()
+        google()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories.addAll(pluginManagement.repositories)
+}
+
 plugins {
     id("com.gradle.develocity") version("3.18.2")
 }
