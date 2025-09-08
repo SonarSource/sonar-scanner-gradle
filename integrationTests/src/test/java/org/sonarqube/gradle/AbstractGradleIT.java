a5ddb07755e3b7c1737a5e5189f9beb0515b5437
lpilastri <115481625+leonardo-pilastri-sonarsource@users.noreply.github.com>
2025-09-08 16:55:14 +0200

    SCANGRADLE-262 Fix FunctionalTests with Windows paths (#343)

--- integrationTests/src/test/java/org/sonarqube/gradle/AbstractGradleIT.java
+++ integrationTests/src/test/java/org/sonarqube/gradle/AbstractGradleIT.java
@@ -116,9 +116,9 @@ public abstract class AbstractGradleIT {
     replacementMap.put("${HOME}", System.getProperty("user.home"));
     String cirrusWorkingDir = System.getenv("CIRRUS_WORKING_DIR");
     if (cirrusWorkingDir != null) {
-      replacementMap.put("${HOME}", cirrusWorkingDir);
+      replacementMap.put("${HOME}", cirrusWorkingDir.replace('/', File.separatorChar));
     }
-    Pattern dependenciesInGradleCache = Pattern.compile("(?<=,|^)/[^,]+/.gradle/caches/modules-\\d+/files-[0-9.]+/" +
+    Pattern dependenciesInGradleCache = Pattern.compile("(?<=,|^)[^,]+/.gradle/caches/modules-\\d+/files-[0-9.]+/" +
       "(?<groupId>[^/,]++)/(?<artifactId>[^/,]++)/(?<version>[^/,]++)/[0-9a-f]{40}/\\k<artifactId>-\\k<version>\\.jar(?=,|$)");
     Map<String, String> result = new LinkedHashMap<>();
     props.stringPropertyNames().stream()
