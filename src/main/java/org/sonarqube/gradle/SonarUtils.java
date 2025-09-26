4db9334b6ea650a903d037a513dc71a8a2218a28
Leonardo Pilastri <leonardo.pilastri@sonarsource.com>
2025-09-26 18:24:10 +0200

    Revert "Remove me later"
    This reverts commit b12da783bc2d878007cc58707af59ccfa6c6af2c.

--- src/main/java/org/sonarqube/gradle/SonarUtils.java
+++ src/main/java/org/sonarqube/gradle/SonarUtils.java
@@ -242,86 +242,16 @@ public class SonarUtils {
   }
 
   public static String joinCsvStringsWithoutDuplicates(String csv1, String csv2) {
-    List
-      <
-        String
-        >
-      list1
-      =
-      splitAsCsv
-        (
-        csv1
-      )
-      ;
-    List
-      <
-        String
-        >
-      list2
-      =
-      splitAsCsv
-        (
-        csv2
-      );
-    Set
-      <
-        String
-        >
+    List<String> list1 = splitAsCsv(csv1);
+    List<String> list2 = splitAsCsv(csv2);
+    Set<String> resultSet = new LinkedHashSet<>();
+    resultSet.addAll(list1);
+    resultSet.addAll(list2);
+    return joinAsCsv(
       resultSet
-      =
-      new
-        LinkedHashSet
-          <
-            >
-        (
-
-        )
-      ;
-    resultSet
-      .
-      addAll
-        (
-      list1
-    )
-    ;
-    resultSet
-      .
-      addAll
-        (
-      list2
-    )
-    ;
-    return
-      joinAsCsv
-      (
-      resultSet
-        .
-        stream
-          (
-        )
-        .
-        filter
-          (
-          s
-            ->
-            !
-              s
-                .
-                isBlank
-                  (
-
-                  )
-        )
-        .
-        collect
-          (
-            Collectors
-              .
-              toList
-                (
-
-                )
-          )
+        .stream()
+        .filter(s -> !s.isBlank())
+        .collect(Collectors.toList())
     );
   }
 
