1a41db99920117f43eedbb9f9c01cca9060a0357
erwan.serandour <erwan.serandour@sonarsource.com>
2025-11-11 17:42:38 +0100

    improve test documentation

--- integrationTests/src/test/java/org/sonarqube/gradle/GradleTest.java
+++ integrationTests/src/test/java/org/sonarqube/gradle/GradleTest.java
@@ -423,13 +423,22 @@ public class GradleTest extends AbstractGradleIT {
       baseDir.resolve(".github"));
   }
 
-  @Test
-  public void testClasspathDependency() throws Exception {
+    /**
+     * SCANGRADLE-293
+     *
+     * WriteToResources affects the classpath, SonarResolverTask depends on the classpath.
+     *
+     * Verify that gradle do not report a conflict.
+     */
+    @Test
+    public void testClasspathDependency() throws Exception {
 
-      // we do not care about the properties just that gradle execute task sonar and return code 0
-      String propertyPath = this.temp.newFile().getAbsolutePath();
-      RunResult result = runGradlewSonarWithEnvQuietly("/java-gradle-classpath-dependency", new HashMap<>(), "-Dsonar.scanner.internal.dumpToFile="+propertyPath);
+        String propertyPath = this.temp.newFile().getAbsolutePath();
+        RunResult result = runGradlewSonarWithEnvQuietly("/java-gradle-classpath-dependency",
+                new HashMap<>(),
+                "-Dsonar.scanner.internal.dumpToFile=" + propertyPath);
 
-      assertThat(result.getExitValue()).isZero();
-  }
+        // verify that gradle do not report a conflict with writeToResources
+        assertThat(result.getExitValue()).isZero();
+    }
 }
