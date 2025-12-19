6f92472b9256ede9512fd3c5cb4e81c5e94ebec4
Gabriel Fleischer <gabriel.fleischer@sonarsource.com>
2025-12-19 10:34:01 +0100

    SCANGRADLE-344 Finalize GitHub Action migration (#427)

--- cirrus/cleanup-gradle-cache.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-rm -rf ~/.gradle/caches/$GRADLE_VERSION/
-rm -rf ~/.gradle/daemon/
-find ~/.gradle/caches/ -name "*.lock" -type f -delete
--- cirrus/prepare-release-configuration-from-cirrus-tag.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Release version: ${CIRRUS_TAG}"
-export CI_BUILD_NUMBER="${CIRRUS_TAG##*.}"
-source cirrus-env RELEASE
-source cirrus/set-version-from-build-number.sh
-cat gradle.properties | grep -E "^version"
