a0b2dad62e4c34f233bf43fcadb7f38f1ccf9034
Dorian Burihabwa <dorian.burihabwa@sonarsource.com>
2026-01-23 11:13:31 +0100

    SCANGRADLE-353 Prevent bootstrapping tests from running when Java version is lower than 21 lower

--- integrationTests/src/test/java/org/sonarqube/gradle/BootstrapTest.java
+++ integrationTests/src/test/java/org/sonarqube/gradle/BootstrapTest.java
@@ -47,7 +47,7 @@ public class BootstrapTest extends AbstractGradleIT {
   public static final OrchestratorRule ORCHESTRATOR;
 
   static {
-    if (getJavaVersion() < 17) {
+    if (getJavaVersion() < 21) {
       ORCHESTRATOR = null;
     } else {
       ORCHESTRATOR = OrchestratorRule.builderEnv()
