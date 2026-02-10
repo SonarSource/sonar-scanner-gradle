ad55d986b4de85d537f8265b262fcd77f8abd1c0
Aurélien Coet <aurelien.coet@sonarsource.com>
2026-02-10 10:10:34 +0100

    Add integration test to check that user defined properties aren't filtered

--- integrationTests/src/test/java/org/sonarqube/gradle/GradleTest.java
+++ integrationTests/src/test/java/org/sonarqube/gradle/GradleTest.java
@@ -148,6 +148,14 @@ public class GradleTest extends AbstractGradleIT {
   }
 
   @Test
+  public void testUserDefinedProperties() throws Exception {
+    Properties props = runGradlewSonarSimulationModeWithEnv("/java-gradle-user-properties", emptyMap(), new DefaultRunConfiguration(), "compileJava", "compileTestJava");
+
+    assertThat(props.getProperty("sonar.sources").split(",")).containsOnly("src/main/java");
+    assertThat(props.getProperty("sonar.tests").split(",")).containsOnly("src/test/java");
+  }
+
+  @Test
   public void mixJavaGroovyProject() throws Exception {
     ignoreThisTestIfGradleVersionIsGreaterThanOrEqualTo("9.0.0");
     Properties props = runGradlewSonarSimulationModeWithEnv("/java-groovy-tests-gradle", emptyMap(), new DefaultRunConfiguration(), "build");
