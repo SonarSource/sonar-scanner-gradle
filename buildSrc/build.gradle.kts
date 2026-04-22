4709e5f1c591dd7527b5c0ec6e55fbb9bf6b3472
Dorian Burihabwa <dorian.burihabwa@sonarsource.com>
2026-04-22 17:17:59 +0200

    SCANGRADLE-397 Upgrade plexus-utils to version 3.6.1
    Kill the noise on dependency risks raised during analysis.

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,5 +7,5 @@ repositories {
 dependencies {
     implementation("com.gradle.publish:plugin-publish-plugin:1.3.1")
     implementation("org.apache.maven:maven-model:3.9.9")
-    implementation("org.codehaus.plexus:plexus-utils:3.6.0")
+    implementation("org.codehaus.plexus:plexus-utils:3.6.1")
 }
