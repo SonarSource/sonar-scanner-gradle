a68ce764d3034f69795da6f233946f4e6dc1dd67
romain.birling <romain.birling@sonarsource.com>
2026-04-23 13:47:59 +0200

    Adjust path normalization for AGP 9 snapshots
    Co-authored-by: Aurelien Coet <aurelien.coet@sonarsource.com>

--- integrationTests/src/test/java/org/sonarqube/gradle/support/normalization/PathsNormalizer.java
+++ integrationTests/src/test/java/org/sonarqube/gradle/support/normalization/PathsNormalizer.java
@@ -49,6 +49,10 @@ public class PathsNormalizer {
     "main", "R.jar", "classes.jar", "classes"
   );
 
+  private static final List<String> IGNORED_PATHS_SUBSTRINGS = List.of(
+    "kotlin-stdlib", "annotations-", "core-runtime-", "lifecycle-common-"
+  );
+
   private PathsNormalizer() {
     // Utility class: contains only static methods and is not intended to be instantiated.
   }
@@ -87,7 +91,7 @@ public class PathsNormalizer {
       }
     }
     return paths
-      .filter(path -> !IGNORED_PATHS.contains(path))
+      .filter(path -> !IGNORED_PATHS.contains(path) && IGNORED_PATHS_SUBSTRINGS.stream().noneMatch(path::contains))
       .distinct()
       .sorted()
       .collect(Collectors.joining(DELIMITER));
